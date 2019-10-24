ea603ca0a8ff16bad4924c4eb95baa572b815aad
Iga <iga.naskret@gmail.com>
2019-10-24 12:22:35 +0200

    files

---
+++ The Stick/JS/thestick.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector(".modal-bg");
+const container = document.querySelector("#container");
+
+container.addEventListener("click", () => showModal());
+
+// modal.addEventListener("click", () => modal.classList.add("hide"));
+
+function showModal() {
+  container.classList.toggle("change");
+
+  if (modal.classList.contains("hide")) {
+    modal.classList.remove("hide");
+  } else {
+    modal.classList.add("slideback");
+    setTimeout(function() {
+      modal.classList.add("hide");
+      modal.classList.remove("slideback");
+    }, 500);
+  }
+  //modal.classList.toggle('hide');
+  //modal.classList.toggle("slideback");
+  // modal.addEventListener("animationend", modal.classList.add("hide"));
+}
